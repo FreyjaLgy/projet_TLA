37967a750361d1316dc3b77e916451c0d003f443
Freyja <freyjalgy@gmail.com>
2023-12-13 14:31:55 +0100

    Table de transitions finie (je pense)

--- src/tla/AnalyseLexicale.java
+++ src/tla/AnalyseLexicale.java
@@ -8,8 +8,9 @@ public class AnalyseLexicale {
             //Là où il y a un null c'est que je sais pas encore quoi mettre
             //           0    1    2       3            4
             //           *    -    )    chiffre  lettre+caractères
-            /*  0 */   {  2, null, null, null, 2},
-            /*  1 */   {105, 2, 2, 2, 2}, //Ligne qui sert à constituer un texte. On continue d'ajouter au texte sauf si il y a un *
+            /*  0 */   {  2, 102, 103,   1,   2},
+            /*  1 */   {104, 104, 104,   1, 104},
+            /*  2 */   {105,   2,   2,   2,   2}, //Ligne qui sert à constituer un texte. On continue d'ajouter au texte sauf si il y a un *
 
             // 101 acceptation d'un *
             // 102 acceptation d'un -
